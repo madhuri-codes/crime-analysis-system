752612973da36b11837aca392d1f08e55288cc1c
Madhuri N <madhuri99980@gmail.com>
2026-04-18 18:39:41 +0530

    Add routes for criminals, suspects, victims, and arrests

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes.js';
+import criminalRoutes from './routes/criminals.js';
+import suspectRoutes from './routes/suspects.js';
+import victimRoutes from './routes/victims.js';
+import arrestRoutes from './routes/arrests.js';
+
 
 dotenv.config();
 
@@ -9,6 +14,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/auth', authRoutes);
+app.use('/api/criminals', criminalRoutes);
+app.use('/api/suspects', suspectRoutes);
+app.use('/api/victims', victimRoutes);
+app.use('/api/arrests', arrestRoutes);
 // test route
 app.get('/', (req, res) => {
   res.json({ message: 'Server is running!' });
