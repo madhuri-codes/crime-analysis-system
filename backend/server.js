de9357d50b872e7295e6e1548c9f5f8dc9d89880
Madhuri N <madhuri99980@gmail.com>
2026-04-18 18:32:47 +0530

    Add authentication routes to the server

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import authRoutes from './routes/authRoutes.js';
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use('/auth', authRoutes);
 // test route
 app.get('/', (req, res) => {
   res.json({ message: 'Server is running!' });
